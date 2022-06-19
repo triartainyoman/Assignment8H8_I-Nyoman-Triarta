27bd1a1e52d5b066e867f1d4a21af4e12935f4b0
Triarta <triarta.art@gmail.com>
2022-06-20 06:55:51 +0800

    Change background color

--- pages/Home.js
+++ pages/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#eaeaea",
     alignItems: "center",
     justifyContent: "center",
   },
